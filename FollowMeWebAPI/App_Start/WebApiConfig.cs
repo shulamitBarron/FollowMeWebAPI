fd96113cfc93b2ca21ae8fda3553c5e39f60c3df
shulamitBarron <shulamitbarron@gmail.com>
2018-10-27 22:45:50 +0300

    Update WebApiConfig.cs

--- FollowMeWebAPI/App_Start/WebApiConfig.cs
+++ FollowMeWebAPI/App_Start/WebApiConfig.cs
@@ -10,9 +10,7 @@ namespace FollowMeWebAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-      EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
-
-      config.EnableCors(cors);
+     
       // Web API routes
       config.MapHttpAttributeRoutes();
 
