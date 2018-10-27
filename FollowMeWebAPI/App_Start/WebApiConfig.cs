8fe5c1579c081bc5a0af825ab512230289fcb817
shulamitBarron <shulamitbarron@gmail.com>
2018-10-27 22:49:02 +0300

    Update WebApiConfig.cs
    ילחילח

--- FollowMeWebAPI/App_Start/WebApiConfig.cs
+++ FollowMeWebAPI/App_Start/WebApiConfig.cs
@@ -1,20 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-
-namespace FollowMeWebAPI
+ namespace FollowMeWebAPI
 {
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-     
-      // Web API routes
-      config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
+             // Web API routes
+            config.MapHttpAttributeRoutes();
+             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
